2e6df29ed8ed81648182437a021f4e2059752f65
yoosangjun <52834205+yoosangjun@users.noreply.github.com>
2021-08-05 17:13:21 +0900

    first

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("my home");
+};
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1> Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
